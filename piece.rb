64f60920312deb86dea8ddae5e93a8620d2ef0ac
R <student@appacademy.io>
2014-01-07 11:27:12 -0500

    Refactors SlidingPiece move method

--- piece.rb
+++ piece.rb
@@ -20,6 +20,54 @@ end
 
 class SlidingPiece < Piece
 
+  def moves
+    deltas = move_dirs
+    position_array = []
+
+    deltas.each do |delta|
+      cur_x, cur_y = @position
+      dx, dy = delta[0], delta[1]
+
+      n = 1
+      loop do
+        pos = [cur_x + n*dx, cur_y + n*dy]
+        break if out_of_bounds?(pos)
+
+        if @board[pos].nil?
+          position_array << pos
+        elsif @board[pos].color != @color
+          position_array << pos
+          break
+        else
+          break
+        end
+        n += 1
+      end
+    end
+    position_array
+  end
+
+  def move_dirs
+    [[1,1], [1,-1], [-1, 1], [-1,-1], [1,0], [-1, 0], [0, 1], [0, -1]]
+  end
+
+  def out_of_bounds?(pos)
+    x,y = pos
+    return false if x.between?(0, 7) && y.between?(0,7)
+    true
+  end
+
+end
+
+=begin
+b=Board.new
+p=SlidingPiece.new([5,5], b, :white)
+b[[5,5]]=p
+
+p2=SlidingPiece.new([3,5], b, :white)
+b[[3,5]]=p2
+=end
+=begin
   def get_horizontals
     position_array = []
     cur_x, cur_y = @position
@@ -125,7 +173,8 @@ class SlidingPiece < Piece
     position_array
 
   end
-end
+=end
+
 
 class SteppingPiece
 
