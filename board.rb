542a1b74b8cd4061e343d153c673253b3d5079bc
R <student@appacademy.io>
2014-01-07 09:55:15 -0500

    Initializes board class

--- board.rb
+++ board.rb
@@ -1,4 +1,16 @@
 class Board
 
+  def initialize
+    @board = Array.new(8) { Array.new(8) { nil } }
+  end
+
+
+  def [](x,y)
+    @board[y][x]
+  end
+
+  def []=(x,y,value)
+    @board[y][x] = value
+  end
 
 end
